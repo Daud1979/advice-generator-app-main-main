826369adc5aafd8e9e2bf49ae7596e76a72f21f4
Daud1979 <100976611+Daud1979@users.noreply.github.com>
2024-05-16 21:56:15 +0200

    finalizado

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // ¡Buena suerte!
+let btn = document.querySelector('.card__dice');
+let ids = document.querySelector('#advice-id');
+let texto = document.querySelector('.card__quote');
+btn.addEventListener('click',async function(){
+    let response = await fetch('https://api.adviceslip.com/advice');
+    let data = await response.json();
+    ids.textContent=data.slip.id;
+    texto.textContent=data.slip.advice;
+
+});
